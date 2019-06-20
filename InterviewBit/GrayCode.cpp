b844feb63e232ed6fda0d78fb0d5544786cc1363
Gagandeep Singh Ahuja <gagandeepahuja09@gmail.com>
2019-06-20 19:28:43 +0530

    easy

--- InterviewBit/GrayCode.cpp
+++ InterviewBit/GrayCode.cpp
@@ -1,26 +1,28 @@
-vector<string> graycodes(int n) {
+vector<string> grayC(int n) {
     if(n == 1) {
-        return { "0" , "1" };
+        return { "0", "1" };
     }
-    vector<string> ans = graycodes(n - 1);
+    vector<string> ans = grayC(n - 1);
     vector<string> res;
     for(int i = 0; i < ans.size(); i++) {
-        if(i % 2 == 0) {
-            res.push_back(ans[i] + "0");
-            res.push_back(ans[i] + "1");
+        if(i % 2) {
+            res.push_back(ans[i] + '1');
+            res.push_back(ans[i] + '0');
         }
         else {
-            res.push_back(ans[i] + "1");
-            res.push_back(ans[i] + "0");
+            res.push_back(ans[i] + '0');
+            res.push_back(ans[i] + '1');
         }
     }
     return res;
 }
 
 vector<int> Solution::grayCode(int A) {
-    vector<string> res = graycodes(A);
     vector<int> ret;
-    for(int i  = 0; i < res.size(); i++) {
+    if(!A)
+        return ret;
+    vector<string> res = grayC(A);
+    for(int i = 0; i < res.size(); i++) {
         unsigned long long num = stoull(res[i], 0, 2);
         ret.push_back((int)num);
     }
